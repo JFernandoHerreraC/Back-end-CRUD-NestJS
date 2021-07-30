86dc70761bbaee073cc8ea2e702ad36c6f70abf5
JFernandoHerreraC <fernando_ver908@hotmail.com>
2021-07-30 13:28:15 -0500

    included use token for users and remove code dry

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersRepository } from './users.repository';
+import { ConfigModule } from '@nestjs/config';
 import { ConfigService } from '@nestjs/config';
 
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersRepository } from './users.repository';
+import { JwtStrategy } from './jwt.strategy';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository]), ConfigService],
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    ConfigModule.forRoot(),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+    ConfigService,
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports:[JwtStrategy, PassportModule],
 })
 export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,23 +5,28 @@ import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UsersRepository)
         private usersRepository: UsersRepository,
-        private configService:ConfigService,
+        private jwtService: JwtService,
+        private configService: ConfigService,
     ) { }
     public async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return await this.usersRepository.createUser(authCredentialsDto);
     }
-    public async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    public async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
         const { username, password } = authCredentialsDto;
-        const user:User = await this.usersRepository.findOne({ username });
-        const passwordCompared:Boolean = await bcrypt.compare(password, user.password);
+        const user: User = await this.usersRepository.findOne({ username });
+        const passwordCompared: Boolean = await bcrypt.compare(password, user.password);
         if (user && passwordCompared) {
-            return 'Success';
+            const payload: JwtPayload = { username };
+            const accessToken: string = await this.jwtService.sign(payload);
+            return { accessToken };
         } else {
             throw new UnauthorizedException('Please, check your login credentials!');
         }
